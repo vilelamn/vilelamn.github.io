bc18e0f4466de7f52cd006e144e160e9e7cd9442
Marcelo <vilelamn@gmail.com>
2023-08-07 07:51:33 -0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,1 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
 
-hamburguer.addEventListener("click", () => {
-    hamburguer.classList.toggle('active');
-    navMenu.classList.toggle('active');
-
-})
